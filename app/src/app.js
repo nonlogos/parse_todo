35404a7e1b0c8a1766d63e1dfb4b251a15b6dda8
Connie Yu <connieyu@key-cy-mbp.local>
2016-07-01 13:56:43 -0500

    completed todoList

--- app/src/app.js
+++ app/src/app.js
@@ -1,36 +1,52 @@
 
 var app = angular.module('myApp', []);
 
-app.run(function() {
-    Parse.initialize('keyapp', 'unused');
-    Parse.serverURL = 'http://localhost:1337';
-
-      // var query = new Parse.Query('Todos');
-      // console.log("query", query)
-      // query.get("vNIIIR5PeB").then(function(objAgain) {
-      //   console.log('obj', objAgain);
-      // }, function(err) {console.error(err); });
-
-      Parse.Cloud.run('averageStars', { movie: 'The Matrix' }).then(function(ratings) {
-  // ratings should be 4.5
-        console.log('averageStars', ratings)
+app.run(function(todos) {
+    Parse.initialize('keyitup', 'unused');
+    Parse.serverURL = 'http://localhost:1337/parse';
 });
 
-})
-
-app.controller('todoCtrl', function($scope) {
+app.controller('todoCtrl', function($scope, todos, $timeout) {
     $scope.todoList = [];
-
+    
     $scope.todoAdd = function() {
-        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+        var newTodo = $scope.todoInput;
+        todos.addTodos(newTodo)
+        .then(function(result) {
+            $scope.todoList[0].id = result.id;
+
+        });
+
+        $scope.todoList.unshift({todoText: newTodo});
+
         $scope.todoInput = "";
+    };
+
+    //function to keep track of checked state of todo item
+    $scope.saveToggleState = function() {
+        var done = this.x.done;
+        var id = this.x.id;
+        todos.saveStatus(id, done);
     };
 
     $scope.remove = function() {
         var oldList = $scope.todoList;
+        var deletedList =[];
         $scope.todoList = [];
         angular.forEach(oldList, function(x) {
             if (!x.done) $scope.todoList.push(x);
+            else deletedList.push(x);
+            todos.deleteTodos(deletedList);
         });
     };
+    
+    this.$onInit = function() {
+        todos.getTodos()
+        .then(function(todoResponse){
+            $timeout(function(){
+                $scope.todoList = todoResponse;
+            }, 0)
+        })
+    }
+
 });
--- app/src/services.js
+++ app/src/services.js
@@ -1,16 +1,99 @@
 (function(){
   'use strict';
 
-  angular.module('todos')
-         .service('todos', []);
+  angular.module('myApp')
+         .factory('todos', function() {
 
-  
+      var addTodos = function(newTodoItem) {
+          var Obj = Parse.Object.extend("todos");
+          var newTodos = new Obj();
 
-    // Promise-based API
-    return {
+          newTodos.set("todoItem", newTodoItem);
+          newTodos.set("done", false);
+
+          return newTodos.save()
+          .then(function(result) {
+            var newObj = {};
+            newObj.id = result.id;
+            newObj.todoText = result.get('todoItem');
+            newObj.done = result.get('done');
+            return newObj;
+          }, function(error) {
+            console.log(error)
+          })
+      };   
+
+    let saveStatus = (id, status) => {
+      const objSave = Parse.Object.extend("todos");
+      let updStatus = new objSave();
+      updStatus.id = id;
+      updStatus.set("done", status);
+      updStatus.save(null, {
+        success: result => result,
+        error: error => {
+          console.log("result", error);
+        }
+      })
+    }
+
+//Deleting todo items
+
+    var deleteTodos = function(deletedList) {
+      var deleteDate = new Date();
+      var Todo = Parse.Object.extend("todos");
       
-      }
-    };
-  }
+      var arrayToDelete = deletedList.map(function(obj){
+        var deletedTodo = new Todo();
+        deletedTodo.id = obj.id;
+        deletedTodo.set('deleteAt', deleteDate);
+        return deletedTodo;
+      });
+      Parse.Object.saveAll(arrayToDelete, {
+        success: function(result) {
+        },
+        error: function(error) {
+          console.error(error);
+        }
+      })
+    }
+
+//Pulling todo items from DB
+    var getTodos = function() {
+      var TodoList = Parse.Object.extend('todos');
+      var Query = new Parse.Query(TodoList);
+
+      Query.doesNotExist("deleteAt");
+      return Query.addDescending('createdAt').find({
+        success: function(result) {
+          return result;
+        },
+        error: function(error) {
+          console.error(error);
+        }
+      })
+      .then(function(result) {
+         return result.map(function(todo) {
+            var todoObj = {};
+            todoObj.todoText =  todo.get('todoItem');
+            todoObj.done = todo.get('done');
+            todoObj.id = todo.id;
+            todoObj.deleteAt = todo.get('deleteAt');
+            return todoObj;
+          }) 
+      })
+    };   
+
+    return {
+      addTodos: addTodos,
+      getTodos: getTodos,
+      saveStatus: saveStatus,
+      deleteTodos: deleteTodos,
+    };      
+
+  });
 
 })();
+
+
+ 
+
